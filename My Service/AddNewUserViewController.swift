9184264bee1f3476eb9bef3c3b6e16787a2f272c
Student12 <Student12@Student12s-iMac.local>
2017-06-15 16:08:45 +0700

    toolbar ok

---
+++ My Service/AddNewUserViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewUserViewController.swift
+//  My Service
+//
+//  Created by Student12 on 6/15/2560 BE.
+//  Copyright © 2560 SNRU. All rights reserved.
+//
+
+import UIKit
+
+class AddNewUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("This ia AddNerUser")
+  
+        
+        
+        
+    } // Main Method
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    } //didRective
+    
+
+
+} //main Class
